f215861d02bd5c09b3100e3c1c1e5139f93a71f9
Annis Zhang <c-dongling.zhang@everbridge.com>
2024-08-22 00:11:43 +0800

    all

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,10 +11,10 @@ export default function Hero() {
       <div className="custom-screen pt-28 text-gray-600">
         <div className="space-y-5 max-w-4xl mx-auto text-center">
           <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl">
-            Generate your next AI QR Code in seconds
+            Generate your next AI FridayBlessing Image in seconds
           </h1>
           <p className="max-w-xl mx-auto">
-            QRGPT makes it simple for you to generate cool looking AI QR codes
+          FridayBlessing-AI makes it simple for you to generate cool looking AI images
             in seconds, completely for free.
           </p>
           <div className="flex items-center justify-center gap-x-3 font-medium text-sm">
@@ -22,7 +22,7 @@ export default function Hero() {
               href="/start"
               className="text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 "
             >
-              Generate your QR Code
+              Generate your Friday Blessing Image
             </NavLink>
             <NavLink
               target="_blank"
