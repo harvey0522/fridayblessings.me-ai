b591579df67bff63419011ee5a5ffb4f6cf2b12c
Annis Zhang <c-dongling.zhang@everbridge.com>
2024-08-25 10:21:41 +0800

    add tests

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,23 @@
-import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Link from 'next/link';
 
-export default function BlogPost() {
-  const router = useRouter();
-  const { id } = router.query;
-
+export default function Blog() {
   return (
     <>
       <Head>
-        <title>Blog Post {id}</title>
+        <title>Blog</title>
       </Head>
       <main style={{ backgroundColor: '#9370DB', minHeight: '100vh', padding: '20px' }}>
-        <h1>Blog Post {id}</h1>
-        <p>这是博客文章 {id} 的内容。</p>
+        <h1>Blog</h1>
+        <p>欢迎来到我们的博客页面！</p>
+        <ul>
+          <li>
+            <Link href="/blog/1">Blog Post 1</Link>
+          </li>
+          <li>
+            <Link href="/blog/2">Blog Post 2</Link>
+          </li>
+        </ul>
       </main>
     </>
   );
